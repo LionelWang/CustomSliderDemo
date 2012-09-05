d7775cd41c24a060be7ba76c3a8531168a6be418
Lionel Wang <wangliang_1013@163.com>
2012-09-05 10:21:56 +0800

    Initial Commit

---
+++ CustomSliderDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomSliderDemo
+//
+//  Created by wangliang on 12-9-5.
+//  Copyright (c) 2012年 wangliang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
